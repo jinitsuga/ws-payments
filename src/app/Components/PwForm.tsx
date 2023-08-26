a1f96b57cfce9bc6f7f78996d30b5bcdc1a88735
agu <iba.agustin@gmail.com>
2023-08-26 14:58:12 -0300

    add wrong password error and rerouting on correct pw

--- src/app/Components/PwForm.tsx
+++ src/app/Components/PwForm.tsx
@@ -1,21 +1,55 @@
 "use client";
-import React from "react";
+import React, { SyntheticEvent, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function PwInput() {
+  const [pwInput, setPwInput] = useState<string>("");
+  const [showErr, setShowErr] = useState<boolean>(false);
+
+  const router = useRouter();
+
+  console.log(pwInput);
+
+  const updatePw = (e: React.FormEvent<HTMLInputElement>) => {
+    setPwInput(e.currentTarget.value);
+  };
+
   return (
-    <form className="w-64">
+    <form className="flex flex-col p-6 rounded-lg">
       <label
-        className=" flex flex-col text-2xl text-left mb-2"
+        className=" flex flex-col text-2xl text-left mb-4 gap-2"
         htmlFor="password"
       >
         Password:
         <input
-          className=" p-2 bg-stone-200 inline rounded-lg border-fuchsia-500 outline-fuchsia-500"
-          placeholder="password"
+          onChange={(e) => {
+            updatePw(e);
+          }}
+          className=" p-2 bg-fuchsia-200 rounded-md"
+          placeholder="enter provided password"
           type="password"
         ></input>
+        <span className={`${showErr ? "block" : "invisible"} text-red-700`}>
+          Wrong password.
+        </span>
       </label>
-      <button className="rounded border-2 p-4 text-2xl">Enter </button>
+      <button
+        onClick={(e: SyntheticEvent) => {
+          e.preventDefault();
+          const pw = process.env.NEXT_PUBLIC_PASS;
+
+          if (pwInput !== pw) {
+            console.log(setShowErr(true));
+          } else if (pwInput === pw) {
+            console.log("you are in");
+            setShowErr(false);
+            router.push("/register");
+          }
+        }}
+        className=" w-24 self-center rounded border-2 p-4 text-2xl"
+      >
+        Enter
+      </button>
     </form>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import PwInput from "./Components/PwForm";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center h-full">
+    <main className="flex gap-4 flex-col items-center justify-center h-full">
+      <h2 className="text-3xl">Register for a showcase</h2>
       <PwInput></PwInput>
     </main>
   );
