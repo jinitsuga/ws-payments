9539f759eb52cc0645f79c59bdf1eba213b1d07b
agu <iba.agustin@gmail.com>
2023-08-26 22:24:45 -0300

    add form structure, form section title

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,3 +1,17 @@
 export default function Register() {
-  return <div>register form</div>;
+  return (
+    <main className="flex flex-col gap-10 text-2xl items-center justify-center h-full">
+      <h2 className="text-4xl flex  ">
+        Join
+        <p className="bg-gradient-to-r font-bold from-ws-pink to-ws-green bg-clip-text text-transparent">
+          {"  Wedding Salon"}
+        </p>
+      </h2>
+      <section className="flex gap-2 h-[400px]">
+        <div className="rounded  w-3/4 p-4">register form</div>
+        <hr className="border-2 border-stone-500 h-full"></hr>
+        <div className="p-4 text-left">marketing text</div>
+      </section>
+    </main>
+  );
 }
